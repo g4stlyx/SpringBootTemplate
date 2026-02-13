50d05f7bc141fe29c02cc3d613cb49fdb3c264ff
g4stlyx <g4stlyonline@gmail.com>
2026-02-13 20:53:56 +0300

    timezone fix. it is now set to Europe/Istanbul instead of UTC -default-.

--- src/main/java/com/g4stly/templateApp/TemplateAppApplication.java
+++ src/main/java/com/g4stly/templateApp/TemplateAppApplication.java
@@ -1,5 +1,6 @@
 package com.g4stly.templateApp;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TemplateAppApplication {
 
 	public static void main(String[] args) {
+		// Set default timezone to Turkey (Istanbul)
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Istanbul"));
 		SpringApplication.run(TemplateAppApplication.class, args);
 	}
 
